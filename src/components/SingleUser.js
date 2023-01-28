19f9fe47001fc616cc2da7effbe5b3b4d985acf5
Akanksha Yadav <programmingwithyadavakanksha.40@gmail.com>
2023-01-28 13:12:53 +0530

    Code refactoring

--- src/components/SingleUser.js
+++ src/components/SingleUser.js
@@ -2,52 +2,54 @@ import React from "react";
 import { useState } from "react";
 import "../App.css";
 
-const SingleUser = ({ name, email, contact, website, address, company, username  }) => {
-  const [buttonText, setbuttonText] = useState(true);
-  const [detailedDes, setDetailDes] = useState("detailed-description-hide")
+const SingleUser = ({ user  }) => {
+  const { name, email, website, phone, address, company, username  } = user;
+  const [descriptionVisible, setDescriptionVisible] = useState(false);
 
-  const handleChange = () => {
-    setbuttonText((prevState) => !prevState);
-    setDetailDes("detailed-description")
+
+  const toggleDescription = () => {
+    setDescriptionVisible((prevState) => !prevState);
   };
     
-  const handleDescription = () => {
-    setDetailDes("detailed-description-hide")
-    setbuttonText(true);
-  }
   return (
-    <div className="detail-wrapper">
-      <h1 className="company-name"> {name} </h1>
-      <div className="contact-wrapper">
-        <h1> website</h1>
-        <h2> {website} </h2>
-      </div>
-      <div className="city-wrapper">
-        <h1> phone </h1>
-        <h2> {contact}</h2>
-      </div>
-      <div className="state-wrapper">
-        <h1> email </h1>
-        <h2> {email} </h2>
-      </div>
-      <button onClick={handleChange}>
-        {buttonText ? "view detail" : "hide detail"}
-      </button>
-      <div className={detailedDes} onClick={handleDescription}>
-      <h1> Description: </h1>
-        <div className="company-wrapper">
-          <h2> Company</h2>
-          <h3>{company} </h3>
+    <div className="company-wrapper">
+      <h2 className="company-name">{name}</h2>
+      <div className="company-detail-wrapper">
+        <div className="company-detail-row-wrapper">
+          <div className="keyvalue-wrapper">
+            <p className="title">Website</p>
+            <p className="value">{website}</p>
+          </div>
+          <div className="keyvalue-wrapper">
+            <p className="title">Phone</p>
+            <p className="value">{phone}</p>
+          </div>
+          <div className="keyvalue-wrapper">
+            <p className="title">Email</p>
+            <p className="value">{email}</p>
+          </div>
         </div>
-        <div className="address-wrapper">
-          <h2> Address</h2>
-          <h3>{address}</h3>
-        </div>
-        <div className="username-wrapper">
-          <h2> Username </h2>
-          <h3> {username} </h3>
-        </div>
+        <button className="button" onClick={toggleDescription}>
+          {descriptionVisible ? "hide detail" : "view detail"}
+        </button>
       </div>
+      {descriptionVisible && <div className="company-description-wrapper">
+        <p className="company-name">Description</p>
+        <div className="company-description-row-wrapper">
+          <div className="keyvalue-wrapper">
+            <p className="title">Company</p>
+            <p className="value">{company.name} </p>
+          </div>
+          <div className="keyvalue-wrapper">
+            <p className="title">Address</p>
+            <p className="value">{address.street}</p>
+          </div>
+          <div className="keyvalue-wrapper">
+            <p className="title">Username</p>
+            <p className="value">{username}</p>
+          </div>
+        </div>
+      </div>}
     </div>
   );
 };

9fc2c615753b1488bb6dc1a05542932037839831
Akanksha Yadav <programmingwithyadavakanksha.40@gmail.com>
2023-01-28 13:12:10 +0530

    Code refactoring

--- src/App.js
+++ src/App.js
@@ -4,34 +4,26 @@ import SingleUser from "./components/SingleUser";
 
 
 function App() {
-  const [usersData, setUsersData] = useState([]);
+  const [users, setUsers] = useState([]);
+  
   const getUsers = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    console.log(response);
     const data = await response.json();
-    console.log(data);
-    setUsersData(data);
+    setUsers(data);
   };
 
   useEffect(() => {
     getUsers();
   }, []);
 
-  console.log(usersData);
-
   return (
     <>
-      {usersData.map((user) => {
+      <h1 className="main-heading">List of users</h1>
+      {users.map((user) => {
         return (
           <SingleUser
             key={user.id}
-            name={user.name}
-            email={user.email}
-            contact={user.phone}
-            website={user.website}
-            company={user.company.name}
-            address={user.address.street}
-            username={user.username}
+            user={user}
           />
         );
       })}
